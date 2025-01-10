dee2795b9fc17ebffd1db22cb1cc3f05fb002017
Kieran Moloney <k.moloney@hotmail.co.uk>
2025-01-10 07:37:21 +0000

    Added helper functions

--- main.py
+++ main.py
@@ -33,3 +33,24 @@ async def transcribe_audio(file: UploadFile = File(...)):
             os.remove(tmp_path)
 
     return result
+
+
+def transcribe_audio_file(file_path: str) -> str:
+    """Transcribe the audio file using whispers API."""
+    with open(file_path, 'r') as audio_file:
+        transcribe_results = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcribe_results.get("text", "")
+
+
+def sumarize_text(transcribe: str) -> str:
+    """Summarize the transcript using ChatGBT"""
+    summary_prompt = f"summarize the following transcript:/n/n{transcript}"
+    completion = openai.ChatCompletion.create(
+        model="gbt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an assistant that sumarizes audio transrciptions."},
+            {"role": "user", "content": summary_prompt}
+        ],
+        temperature=0.5,
+    )
+    return completion.choice[0].message["content"]
